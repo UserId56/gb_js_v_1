3844d64deb504d69a36cea1c08f8469b39a1a24c
UserId56 <zaregay_menay@mail.ru>
2021-02-02 20:14:43 +0700

    Выполение ДЗ 1 урока

---
+++ js/lesson_1.js
@@ -0,0 +1,10 @@
+let tC = prompt("Введите Tс")
+let tF = (9 / 5) * tC + 32
+alert("Температура Tf: " + tF)
+
+let admin
+let name = "Василий"
+
+admin = name
+alert("Значение переменной admin = " + admin)
+alert('JS-выражение 1000 + "108 " будет равно 1000108')
